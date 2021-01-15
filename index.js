863bde2389d408fd41bbc8e6d388deedcae6fdea
Hayk Mikayelyan <hmikayelyan@NROC-DP-HMIKA.local>
2021-01-15 16:54:51 -0500

    fix

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 #include "cc.js";
 #include "colors-helper.js";
 
-/*
-var score1 = rgb([0, 0, 0], [255, 255, 255]);
-alert(score1); // = 21
-alert(score(score1));
-
-*/
-
-
 var doc = app.activeDocument;
 
 //Array of every single pageItem in the document
@@ -23,6 +15,7 @@ while ( n-- ) {
 n = tfs.length;
 alert( n + " textframes have been found" );
 
+// Looping through text frames
 while (n--) {
 	var textFrame = tfs[n];
 	var txt = textFrame.texts[0].fillColor
@@ -31,10 +24,12 @@ while (n--) {
 		var textHex = cmykToHex(txt.colorValue[0], txt.colorValue[1], txt.colorValue[2], txt.colorValue[3]);
 		alert(textHex);
 
-		// find background color HEX and run contras ratio check against textHex
+		// Find backgroundHex and run contrast ratio check against textHex
 
 		// var score2 = hex(textHex, backgroundHex);
 		// alert(score(score2));
+
+		// Get the ratio and parse the results
 
 	} else {
 		// ..
@@ -43,3 +38,11 @@ while (n--) {
 
 	
 }
+
+
+
+/*
+var score1 = rgb([0, 0, 0], [255, 255, 255]);
+alert(score1); // = 21
+alert(score(score1));
+*/
