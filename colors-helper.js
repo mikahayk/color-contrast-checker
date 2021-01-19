7e1761b5d8fd0c8db0c2ee5291f835dc5e157107
Hayk Mikayelyan <hmikayelyan@NROC-DP-HMIKA.lan>
2021-01-19 14:52:39 -0500

    added background graphics

--- colors-helper.js
+++ colors-helper.js
@@ -9,22 +9,30 @@ function cmykToHex(c, m, y, k) {
 }
 
 function cmykToRgb(c, m, y, k) {
+    c = (c / 100);
+    m = (m / 100);
+    y = (y / 100);
+    k = (k / 100);
+    
+    c = c * (1 - k) + k;
+    m = m * (1 - k) + k;
+    y = y * (1 - k) + k;
+    
+    var r = 1 - c;
+    var g = 1 - m;
+    var b = 1 - y;
+    
 
-    var rgb_r,
-        rgb_g,
-        rgb_b,
-        cyan = 100 * Number(c),
-        magenta = 100 * Number(m),
-        yellow = 100 * Number(y),
-        black = 100 * Number(m);
-    0 < cyan ? cyan /= 100 : 0 < magenta ? magenta /= 100 : 0 < yellow ? yellow /= 100 : 0 < black && (black /= 100);
-    rgb_r = 1 - Math.min(1, cyan * (1 - black) + black);
-    rgb_g = 1 - Math.min(1, magenta * (1 - black) + black);
-    rgb_b = 1 - Math.min(1, yellow * (1 - black) + black);
-    rgb_r = Math.round(255 * rgb_r);
-    rgb_g = Math.round(255 * rgb_g);
-    rgb_b = Math.round(255 * rgb_b);
-    return { r: rgb_r, g: rgb_g, b: rgb_b };
+    r = Math.round(255 * r);
+    g = Math.round(255 * g);
+    b = Math.round(255 * b);
+
+    
+    return {
+        r: r,
+        g: g,
+        b: b
+    }
 }
 
 function rgbToHex(r, g, b) {
--- index.js
+++ index.js
@@ -1,48 +1,70 @@
 #include "cc.js";
 #include "colors-helper.js";
 
+
 var doc = app.activeDocument;
 
 //Array of every single pageItem in the document
 var myItems = doc.allPageItems;
-var  n = myItems.length, tfs = [], nItem;
+var  n = myItems.length, tfs = [], nonTextObjects = [], objOverlaps = [], nItem;
 
 //Looping through page items to collect text frames
 while ( n-- ) {
 	nItem = myItems[n];
-	(nItem instanceof TextFrame) && tfs.push ( nItem );
+	if(nItem instanceof TextFrame) {
+		tfs.push ( nItem );
+	} else {
+		nonTextObjects.push ( nItem );
+	}
 }
-n = tfs.length;
-alert( n + " textframes have been found" );
 
-// Looping through text frames
+n = tfs.length;
+//alert( n + " textframes have been found" );
+$.writeln(n + " textframes have been found \n" );
+
+// Looping through text frames and identify overlaps
 while (n--) {
 	var textFrame = tfs[n];
-	var txt = textFrame.texts[0].fillColor
-	
-	if (String(txt.space) == 'CMYK') {
-		var textHex = cmykToHex(txt.colorValue[0], txt.colorValue[1], txt.colorValue[2], txt.colorValue[3]);
-		alert(textHex);
-
-		// Find backgroundHex and run contrast ratio check against textHex
-
-		// var score2 = hex(textHex, backgroundHex);
-		// alert(score(score2));
-
-		// Get the ratio and parse the results
-
-	} else {
-		// ..
-		// TO DO
-	}
-
-	
+	txtFrameBounds = textFrame.geometricBounds;
+	l = nonTextObjects.length;
+	while (l--) {
+		obj = nonTextObjects[l];
+		objBounds = obj.geometricBounds;
+		// Check if the txtFrame overlaps with obj's bounds
+		if (!(objBounds[0] > txtFrameBounds[2] ||
+				objBounds[2] < txtFrameBounds[0] ||
+				objBounds[1] > txtFrameBounds[3] ||
+				objBounds[3] < txtFrameBounds[1])) {
+			
+			// Save overlaps into objOverlaps in pairs { obj, textFrame }
+			var element = {};
+			element.obj = obj;
+			element.textFrame = textFrame;
+			objOverlaps.push(element);
+		}
+	}	
 }
 
+// Looping through overlaps and perform contrast checks
+n = objOverlaps.length;
+while (n--) { 
 
+	var txt = objOverlaps[n].textFrame.texts[0].fillColor
+	var graphic = objOverlaps[n].obj.fillColor;
 
-/*
-var score1 = rgb([0, 0, 0], [255, 255, 255]);
-alert(score1); // = 21
-alert(score(score1));
-*/
+	if (String(txt.space) == 'CMYK') {
+		var textHex = cmykToHex(txt.colorValue[0], txt.colorValue[1], txt.colorValue[2], txt.colorValue[3]);
+		$.writeln("Text color: ", textHex);
+	}
+	
+	if (String(graphic.space) == 'CMYK') {
+		var graphicHex = cmykToHex(graphic.colorValue[0], graphic.colorValue[1], graphic.colorValue[2], graphic.colorValue[3]);
+		$.writeln("Bakcground graphic color: ", graphicHex);
+	}
+
+	// Calculate the ratio
+	var ratio = hex(textHex, graphicHex);
+	$.writeln("CC ratio between ", textHex, " and ", graphicHex, "is ", ratio);
+	$.writeln("Status: ", score(ratio), "\n");
+
+}
